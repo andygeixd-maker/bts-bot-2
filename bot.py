7a769243c2fe078c68537e3d25454865288a0655
andygeixd-maker <andygeixd@gmail.com>
2026-04-27 14:49:55 -0500

    bot.py

--- bot.py
+++ bot.py
@@ -149,8 +149,22 @@ class ControlView(discord.ui.View):
         super().__init__(timeout=None)
 
   
+    @discord.ui.button(label="FORCE CHECK", style=discord.ButtonStyle.success)
+    async def force_private(self, interaction: discord.Interaction, button: discord.ui.Button):
+
+        state = check_site()
+
+        # SOLO QUIEN PRESIONA LO VE
+        await interaction.response.send_message(
+            f"🔎 Estado del sitio: {state}",
+            ephemeral=True
+        )
+
+        if state == "yes":
+            await interaction.channel.send(embed=make_embed())
+
     @discord.ui.button(label="TEST ALERT", style=discord.ButtonStyle.primary)
-    async def test(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def force_private(self, interaction: discord.Interaction, button: discord.ui.Button):
 
         await interaction.response.send_message(
             "🔥 Test ejecutado",
